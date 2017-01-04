4ba914ac4901de45fcb5ade45f0bc53e7dbcaff2
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-01-04 19:37:32 +0800

    首頁基本排版完成 TODO:  加入隨選飲料店資訊 TODO: 重構程式碼

--- front-end/drink_lover/www/js/index.js
+++ front-end/drink_lover/www/js/index.js
@@ -1,19 +1,16 @@
 (function () {
     "use strict";
 
-    // Initialize your app
+    //TODO: 重構程式碼
+
     var myApp = new Framework7({
         animateNavBackIcon: true
     });
 
-    // Export selectors engine
     var $$ = Dom7;
 
-    // Add main View
     var mainView = myApp.addView('.view-main', {
-        // Enable dynamic Navbar
         dynamicNavbar: true,
-        // Enable Dom Cache so we can use all inline pages
         domCache: true
     });
 
