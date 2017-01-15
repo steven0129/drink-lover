89af1a5e85fa421f453292f5f1b1a6f8d056449a
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-01-15 13:03:24 +0800

    刪除後端程式碼

--- back-end/api/DBconn.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    // "get": function (req, res, next) {
-    // }
-    "get": function(req, res, nest) {
-        var date = { currentTime: Date.now() };
-        res.status(200).type('application/json').send(date);
-    }
-}
--- back-end/api/placesinfo.js
+++
@@ -1,9 +0,0 @@
-var request=require('request');
-module.exports = {
-    "post": function(req, res, next) {
-        request('https://maps.googleapis.com/maps/api/place/textsearch/json?location='+ req.body.location +'&query='+ encodeURIComponent('飲料店') +'&language=zh-TW&key=AIzaSyB1FeVNtCVEF6QKTquVuRYdX5IxInJZuYY', function (error, response, body) {
-             if(error) res.json('{"error":"' + error + '"}');
-             else res.send(body);
-        });
-    }
-}
